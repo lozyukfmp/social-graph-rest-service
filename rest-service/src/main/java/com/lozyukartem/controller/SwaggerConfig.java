a99c21ee7bbb18f6c39d62323c083fbae66f8edf
arla0217 <artemlozyuk@gmail.com>
2017-04-11 16:59:00 +0300

    fix swagger

--- rest-service/src/main/java/com/lozyukartem/controller/SwaggerConfig.java
+++ rest-service/src/main/java/com/lozyukartem/controller/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.lozyukartem.controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -12,6 +13,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
+    @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .groupName("Social Graph")
